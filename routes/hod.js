2ddb6e035ac7acc7eb8fa0613643e05e71bca0f3
Murugavel V <muruvel19.10@gmail.com>
2023-09-04 13:56:09 +0530

    added add dean feature

--- routes/hod.js
+++ routes/hod.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const hodController = require('../controllers/hod_controller');
 
-router.get('/sign-up', hodController.loadSignin);
+router.get('/sign-up', hodController.loadSignup);
 router.get('/login', hodController.loadLogin);
 router.get('/dashboard', hodController.loadPage);
 router.post('/create', hodController.createUser);
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const homeController = require('../controllers/home_controller');
 router.get('/', homeController.loadHome);
 router.use('/roles', require('./roles'));
 router.use('/hod', require('./hod'));
+router.use('/dean', require('./dean'));
 
 module.exports = router;
